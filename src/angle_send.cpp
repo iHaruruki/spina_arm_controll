53fdb10e5482aede1b49312df7249a100dd2911f
Haruki <haruki.isono861@gmail.com>
2025-04-29 12:01:26 +0900

    fix

--- src/angle_send.cpp
+++ src/angle_send.cpp
@@ -1,43 +1,71 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
-#include <string>
+#include <array>
+#include <iomanip>
+#include <sstream>
+#include <chrono>
 
 // 各モジュールの角度を設定(-30~30)
-#define angle_m1 10
-#define angle_m2 -10
-#define angle_m3 10
-#define angle_m4 -10
-#define angle_m5 10
-#define angle_m6 10
+#define angle_m1 -10
+#define angle_m2 -15
+#define angle_m3 -25
+#define angle_m4 -25
+#define angle_m5 -25
+#define angle_m6 -25
 
-int main(int argc,char **argv)
+using namespace std::chrono_literals;
+
+class AngleSendNode : public rclcpp::Node
 {
-    // Initialize Node
-    rclcpp::init(argc,argv);
-    // Create Node
-    auto node =rclcpp::Node::make_shared("angle_send_node");
-    // Create Topic
-    auto pub_cmd = node->create_publisher<std_msgs::msg::String>("angle_cmd", 1);
+public:
+  AngleSendNode()
+  : Node("angle_send_node"),
+    id_(1),
+    angles_{ angle_m1, angle_m2, angle_m3, angle_m4, angle_m5, angle_m6 }  // m1…m6 の角度
+  {
+    pub_ = this->create_publisher<std_msgs::msg::String>("angle_cmd", 1);
 
-    //ループ周期の設定
-    rclcpp::WallRate loop(0.5); //0.5Hz
+    // 1秒ごとに publish_angle() を呼ぶタイマー
+    timer_ = this->create_wall_timer(
+      1s, std::bind(&AngleSendNode::publish_angle, this));
+  }
 
-    int id = 0;
-    int angle[6] = {angle_m1, angle_m2, angle_m3, angle_m4, angle_m5, angle_m6};
-
-    //各モジュールに角度を送信
-    while(rclcpp::ok())
-    {
-        id++;
-        auto msg = std_msgs::msg::String();
-        msg.data = std::string("C") + std::to_string(id) + "p" + std::to_string(angle[id-1]);
-        pub_cmd->publish(msg);
-        RCLCPP_INFO(node->get_logger(), "Publish: %s (module:%d)",msg.data.c_str(),id);
-        if(id == 6) break;
-        loop.sleep();
+private:
+  void publish_angle()
+  {
+    if (id_ > 6) {
+      RCLCPP_INFO(get_logger(), "All messages sent, shutting down.");
+      rclcpp::shutdown();
+      return;
     }
 
-    // Shutdown Connection
-    rclcpp::shutdown();
-    return 0;
+    // 例: "C1p010" / "C2p-010" のように 3 桁ゼロ埋めにしたい場合
+    std::ostringstream ss;
+    ss << 'C' << id_
+       << 'p'
+       << (angles_[id_-1] < 0 ? '-' : '+')
+       << std::setw(3) << std::setfill('0')
+       << std::abs(angles_[id_-1]);
+
+    auto msg = std_msgs::msg::String();
+    msg.data = ss.str();
+
+    pub_->publish(msg);
+    RCLCPP_INFO(get_logger(), "Publish: %s (module:%d)",
+                msg.data.c_str(), id_);
+
+    ++id_;
+  }
+
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  int id_;
+  std::array<int,6> angles_;
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<AngleSendNode>());
+  return 0;
 }
