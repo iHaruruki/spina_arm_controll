1c75b68e318487edcdd7f5204f978d1ded44583d
Haruki <haruki.isono861@gmail.com>
2025-05-05 11:22:52 +0900

    add: posture_

--- src/angle_send.cpp
+++ src/angle_send.cpp
@@ -1,9 +1,19 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 #include <array>
+#include <vector>
+#include <string>
 #include <iomanip>
 #include <sstream>
 #include <chrono>
+
+// 各モジュールの姿勢の軸設定
+#define posture_m1 p
+#define posture_m2 p
+#define posture_m3 p
+#define posture_m4 p
+#define posture_m5 p
+#define posture_m6 p
 
 // 各モジュールの角度を設定(-30~30)
 #define angle_m1 20
@@ -20,52 +30,66 @@ using namespace std::chrono_literals;
 class AngleSendNode : public rclcpp::Node
 {
 public:
-  AngleSendNode()
-  : Node("angle_send_node"),
-    id_(1),
-    angles_{ angle_m1, angle_m2, angle_m3, angle_m4, angle_m5, angle_m6 }  // m1-m6 の角度
-  {
-    pub_ = this->create_publisher<std_msgs::msg::String>("angle_cmd", 1);
+    AngleSendNode()
+    : Node("angle_send_node"),
+      id_(1),
+      angles_{ angle_m1, angle_m2, angle_m3, angle_m4, angle_m5, angle_m6 }
+    {
+        // パラメータ "axes" を宣言（6個分。デフォルトはすべて "p"）
+        auto default_axes = std::vector<std::string>(6, "p");
+        std::vector<std::string> axes_param = this->declare_parameter("axes", default_axes);
 
-    timer_ = this->create_wall_timer(
-      std::chrono::seconds(period), std::bind(&AngleSendNode::publish_angle, this));
-  }
+        for (size_t i = 0; i < 6; i++) {
+            if (i < axes_param.size() && !axes_param[i].empty())
+                axes_[i] = axes_param[i][0];
+            else
+                axes_[i] = 'p';
+        }
 
-private:
-  void publish_angle()
-  {
-    if (id_ > 6) {
-      RCLCPP_INFO(get_logger(), "All messages sent, shutting down.");
-      rclcpp::shutdown();
-      return;
+        pub_ = this->create_publisher<std_msgs::msg::String>("angle_cmd", 1);
+
+        timer_ = this->create_wall_timer(
+            std::chrono::seconds(period),
+            std::bind(&AngleSendNode::publish_angle, this));
     }
 
-    std::ostringstream ss;
-    ss << 'C' << id_
-       << 'p'
-       << (angles_[id_-1] < 0 ? '-' : '+')
-       << std::setw(3) << std::setfill('0')
-       << std::abs(angles_[id_-1]);
+private:
+    void publish_angle()
+    {
+        if (id_ > 6) {
+            RCLCPP_INFO(get_logger(), "All messages sent, shutting down.");
+            rclcpp::shutdown();
+            return;
+        }
 
-    auto msg = std_msgs::msg::String();
-    msg.data = ss.str();
+        std::ostringstream ss;
+        ss << 'C' << id_
+           << axes_[id_ - 1]
+           << (angles_[id_ - 1] < 0 ? '-' : '+')
+           << std::setw(3) << std::setfill('0')
+           << std::abs(angles_[id_ - 1]);
 
-    pub_->publish(msg);
-    RCLCPP_INFO(get_logger(), "Publish: %s (module:%d)",
-                msg.data.c_str(), id_);
+        auto msg = std_msgs::msg::String();
+        msg.data = ss.str();
 
-    ++id_;
-  }
+        pub_->publish(msg);
+        RCLCPP_INFO(get_logger(), "Publish: %s (module:%d)",
+                     msg.data.c_str(), id_);
 
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  int id_;
-  std::array<int,6> angles_;
+        ++id_;
+    }
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    int id_;
+    std::array<int, 6> angles_;
+    std::array<char, 6> axes_;
 };
 
 int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<AngleSendNode>());
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<AngleSendNode>());
+    rclcpp::shutdown();
+    return 0;
 }
