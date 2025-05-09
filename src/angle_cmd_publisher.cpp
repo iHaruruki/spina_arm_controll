3708ca35c7c2c0b28db0cb6db2b3025b64a27b8b
Haruki <haruki.isono861@gmail.com>
2025-05-09 19:44:52 +0900

    add: angle_cmd_publisher

---
+++ src/angle_cmd_publisher.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <memory>
+#include <string>
+#include <vector>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+class AngleCmdPublisher : public rclcpp::Node
+{
+public:
+    AngleCmdPublisher()
+        : Node("angle_cmd_publisher"), messages_({"C3p-020", "C3r+020", "C3p+020", "C3r-020"}), current_index_(0)
+    {
+        publisher_ = this->create_publisher<std_msgs::msg::String>("/angle_cmd", 10);
+        timer_ = this->create_wall_timer(1s, std::bind(&AngleCmdPublisher::publish_message, this));
+    }
+
+private:
+    void publish_message()
+    {
+        auto message = std_msgs::msg::String();
+        message.data = messages_[current_index_];
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        publisher_->publish(message);
+
+        // Move to the next message in the list
+        current_index_ = (current_index_ + 1) % messages_.size();
+    }
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    std::vector<std::string> messages_;
+    size_t current_index_;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<AngleCmdPublisher>());
+    rclcpp::shutdown();
+    return 0;
+}
